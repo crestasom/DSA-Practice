4dae19bb2261f50c71f138ac78f4e80314f08abc
Som Prasad Shrestha <crestasom@gmail.com>
2018-05-07 21:34:05 +0545

    revision question added

--- Pointers/pointer array.cpp
+++ Pointers/pointer array.cpp
@@ -1,15 +1,15 @@
 #include<stdio.h>
 #include<stdlib.h>
 main(){
-//	int *ptr,n,i;
-	printf("Enter size of array");
-//	scanf("%d",&n);
-//	ptr=(int*)malloc(n*sizeof(int));
-//	for(i=0;i<n;i++){
-//		printf("Enter element:%d",i);
-//		scanf("%d",ptr++);
-//	}
-//	for(i=0;i<n;i++){
-//		printf("%d\n",*ptr++);
-//	}
+	int *ptr,n,i;
+	printf("Enter size of array:");
+	scanf("%d",&n);
+	ptr=(int*)malloc(n*sizeof(int));
+	for(i=0;i<n;i++){
+	printf("Enter element%d:",i);
+		scanf("%d",ptr+i);
+	}
+	for(i=0;i<n;i++){
+		printf("%d\n",*(ptr+i));
+	}
 }
